74de1ee395f80258db5e3d74a27782cae8856358
Greta <asth0010@elev.techcollege.dk>
2025-10-08 08:39:06 +0200

    new

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,11 @@
-// her har vi følgende produkter
-//   getProducts()
+// her har vi følgende funktioner
+
+//   getProducts()  
 //   getCategories()
 //   getProductById(id)
 //   searchProducts(query)
+
+export function getProducts(){ fetch('https://dummyjson.com/products')
+    .then(res => res.json()) //response
+    .then(console.log);} // alt ind i funktion, skal være i curlybrackets
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@
 
 //    Entry point for appen
 //    Initialiserer appen og loader data
+
+import { getProducts } from "./api.js";
+
+getProducts() // dette er til at kalde funktionen, så den kører engang
